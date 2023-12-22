d532c5e69c3aa9961d59f35b1f31d8c27f0f1c9d
LAPTOP-91DON713\janardandhavan <tejaswibhokare17@gmail.com>
2023-12-22 00:19:21 -0500

    Added intro and skills section with css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,7 @@ import TwitterIcon from '@mui/icons-material/Twitter';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import "../styles/Footer.css"
 
 
 
@@ -12,7 +13,7 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 function Footer() {
     return (
         <div className="footer">
-            <div>
+            <div className="socialMedia">
                 <InstagramIcon/>
                 <TwitterIcon/>
                 <FacebookIcon/>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,41 @@
 import React from 'react'
+import EmailIcon from '@mui/icons-material/Email';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import "../styles/Home.css"
 
 function Home() {
     return (
-        <div>
-          Home  
+        <div className="home">
+          <div className="about">
+
+            <h2>Hi, My Name is Tejaswi</h2>
+            <div className="prompt"> 
+            <p> A Software Developer with a passion for learning and creating.</p>
+            <LinkedInIcon/>
+            <EmailIcon/>
+            <GitHubIcon/>
+            </div>
+          </div>
+
+          <div className="skills">
+            <h1>Skills</h1>
+            <ol className="list">
+              <li className="item">
+                <h2>Front-End</h2>
+                <span> ReactJS, Redux, HTML, CSS, NPM, NextJS</span>
+              </li>
+              <li className="item">
+                <h2>Back-End</h2>
+                <span>NodeJS, ExpressJS, MongoDB, SQL </span>
+              </li>
+              <li className="item">
+                <h2>Langauges</h2>
+                <span> JavaScript, TypeScript </span>
+              </li>
+
+            </ol>
+          </div>
         </div>
     )
 }
