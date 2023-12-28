c6b271a420697f26f1a433f6ad552f8a25c8c16f
LAPTOP-91DON713\janardandhavan <tejaswibhokare17@gmail.com>
2023-12-27 21:12:38 -0500

    added project navigation

--- src/pages/ProjectDisplay.js
+++ src/pages/ProjectDisplay.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import { ProjectList} from "../helpers/ProjectList"
+import GitHubIcon from '@mui/icons-material/GitHub';
+import "../styles/ProjectDisplay.css"
 
 function ProjectDisplay() {
 
     const { id } = useParams()
+    const project = ProjectList[id]
     return (
-        <div>
-            { id }
+        <div className="project">
+            <h1> { project.name }</h1>
+            <img src={project.image} alt="projects" />
+            <p>
+                <b>Skills :</b> {project.skills}
+            </p>
+            <GitHubIcon />
         </div>
     )
 }
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -3,15 +3,15 @@ import ProjectItem from "../components/ProjectItem"
 
 import { ProjectList} from '../helpers/ProjectList'
 
-import "../styles/Project.css"
+import "../styles/Projects.css"
 
 function Projects() {
     return (
         <div className="projects">
           <h1> My Personal Projects</h1>
           <div className="projectList">
-              {ProjectList.map((project)=>  {
-                return <ProjectItem name={project.name} image={project.image}/>
+              {ProjectList.map((project, idx)=>  {
+                return <ProjectItem id={idx} name={project.name} image={project.image}/>
               })}
 
           </div>
