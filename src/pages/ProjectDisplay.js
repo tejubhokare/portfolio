710369a69fb2a977c7dfe5a9eb6c1c33d64c2193
LAPTOP-91DON713\janardandhavan <tejaswibhokare17@gmail.com>
2023-12-27 20:20:10 -0500

    added project display

---
+++ src/pages/ProjectDisplay.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+function ProjectDisplay() {
+
+    const { id } = useParams()
+    return (
+        <div>
+            { id }
+        </div>
+    )
+}
+
+export default ProjectDisplay
